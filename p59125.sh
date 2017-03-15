505ce6320926c33cb82992eb48d0cfaec4104bd0
Pranjal Bhor <bhor.pranjal@gmail.com>
2017-03-15 19:07:29 +0530

    Added 4 server spawning .sh files

--- p59125.sh
+++ p59125.sh
@@ -1,3 +1,1 @@
-export MARY_BASE=/home/murtraja/Downloads/marytts_downloads/releases/marytts-5.2
-
 java -showversion -Xms40m -Xmx1g -cp "$MARY_BASE/lib/*" -Dmary.base="$MARY_BASE" -Dsocket.port=59125 $* marytts.server.Mary
--- run_mary_servers.sh
+++ run_mary_servers.sh
@@ -1,4 +1,4 @@
-export MARY_BASE=/home/murtraja/Downloads/marytts_downloads/releases/marytts-5.2
+export MARY_BASE=/home/pranjal/marytts-5.2/target/marytts-5.2/
 gnome-terminal -e "bash p59125.sh"
 gnome-terminal -e "bash p59126.sh"
 gnome-terminal -e "bash p59127.sh"
