55ce0cb7c19ea02a5ce693ad8f23be3ea4818b2f
MMR <mmrxyz@gmail.com>
2017-03-17 07:03:35 +0530

    server spawner takes MARY_BASE as argument

--- run_mary_servers.sh
+++ run_mary_servers.sh
@@ -1,5 +1,9 @@
-export MARY_BASE=/home/pranjal/marytts-5.2/target/marytts-5.2/
-gnome-terminal -e "bash p59125.sh"
-gnome-terminal -e "bash p59126.sh"
-gnome-terminal -e "bash p59127.sh"
-gnome-terminal -e "bash p59128.sh"
+if [ "$#" -eq 0 ];then
+	echo "please enter the MARY_BASE as a command line argument"
+else
+	export MARY_BASE=$1
+	gnome-terminal -e "bash p59125.sh"
+	gnome-terminal -e "bash p59126.sh"
+	gnome-terminal -e "bash p59127.sh"
+	gnome-terminal -e "bash p59128.sh"
+fi
